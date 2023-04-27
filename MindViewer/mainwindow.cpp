0da2e5ac5fdb853f276177f9e03e28cb7e1962de
Jackey Lea <1768478912@qq.com>
2023-04-27 22:23:33 +0800

    设置指示器界面

--- MindViewer/mainwindow.cpp
+++ MindViewer/mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->widgetAttention->setLabel("Attention");
+    ui->widgetMeditation->setLabel("Meditation");
 }
 
 MainWindow::~MainWindow()
