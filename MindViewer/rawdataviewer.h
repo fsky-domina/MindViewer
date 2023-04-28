f8d96113a1d32dd12ee3dae8f0e015b812169736
Jackey Lea <1768478912@qq.com>
2023-04-28 21:33:47 +0800

    添加原始数据显示代码

--- MindViewer/rawdataviewer.h
+++ MindViewer/rawdataviewer.h
@@ -9,6 +9,13 @@
 
 #include <QObject>
 #include <QWidget>
+#include <QPaintEvent>
+#include <QPointF>
+
+#include "qwt_plot.h"
+#include "qwt_plot_canvas.h"
+#include "qwt_plot_curve.h"
+#include "qwt_plot_layout.h"
 
 class RawDataViewer : public QWidget
 {
@@ -16,8 +23,25 @@ class RawDataViewer : public QWidget
 public:
     explicit RawDataViewer(QWidget *parent = nullptr);
 
+    void setMaxDataCnt(uint cnt);
+
+    void appendData(double data);//添加新数据
+
+protected:
+    void paintEvent(QPaintEvent *e) override;
+
 signals:
 
+private:
+    QwtPlotCurve curveRaw;//折线
+
+    QVector<double> m_xdata;//x坐标
+    //曲线数据
+    QVector<double> m_data;
+
+    QwtScaleMap xMap,yMap;
+
+    uint m_cnt;//最大数据量
 };
 
 #endif // RAWDATAVIEWER_H
