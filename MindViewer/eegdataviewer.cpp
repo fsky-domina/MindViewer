421bcb174cab2501f178e05d71e77436e6cba67a
Jackey Lea <1768478912@qq.com>
2023-04-29 21:45:44 +0800

    添加eeg功能

--- MindViewer/eegdataviewer.cpp
+++ MindViewer/eegdataviewer.cpp
@@ -1,7 +1,41 @@
 #include "eegdataviewer.h"
 
-EEGDataViewer::EEGDataViewer(QWidget *parent)
-    : QWidget{parent}
-{
+const QwtInterval s_radialInterval(0,10);
+//const QwtInterval s_radialInterval(-32768,32767);
+const QwtInterval s_azimuthInterval(0,360);
 
+EEGDataViewer::EEGDataViewer(QWidget *parent)
+    : QwtPolarPlot(parent)
+{
+    setScale(QwtPolar::Radius,s_radialInterval.minValue(),s_radialInterval.maxValue());
+    setScale(QwtPolar::Azimuth,s_azimuthInterval.minValue(),s_azimuthInterval.maxValue(),s_azimuthInterval.width() / 8);
+
+    //网格
+    m_grid = new  QwtPolarGrid();
+    m_grid->setPen(QPen(Qt::black));
+    m_grid->showAxis( QwtPolar::AxisAzimuth, false );
+    m_grid->showAxis( QwtPolar::AxisLeft, false );
+    m_grid->showAxis( QwtPolar::AxisRight, false );
+    m_grid->showAxis( QwtPolar::AxisTop, false );
+    m_grid->showAxis( QwtPolar::AxisBottom, false );
+    m_grid->showGrid( QwtPolar::Azimuth, true );
+    m_grid->showGrid( QwtPolar::Radius, true );
+    m_grid->attach( this );
+
+    //数据
+    data = new EEGData();
+
+    //曲线
+    m_curve = new QwtPolarCurve();
+    m_curve->setStyle(QwtPolarCurve::Lines);
+    m_curve->setPen(QPen(Qt::green,2));
+    m_curve->setSymbol( new QwtSymbol( QwtSymbol::Rect,
+         QBrush( Qt::cyan ), QPen( Qt::white ), QSize( 3, 3 ) ) );
+    m_curve->setData(data);
+    m_curve->attach(this);
+}
+
+void EEGDataViewer::updateData(QVector<double> eeg)
+{
+    data->updateData(eeg);
 }
--- MindViewer/eegdataviewer.h
+++ MindViewer/eegdataviewer.h
@@ -1,17 +1,80 @@
+///////////////////////
+/// JackeyLea
+/// 20230428
+/// 用于显示EEG数据
+///////////////////////
+
 #ifndef EEGDATAVIEWER_H
 #define EEGDATAVIEWER_H
 
 #include <QObject>
 #include <QWidget>
+#include <QDebug>
 
-class EEGDataViewer : public QWidget
+#include "QwtPolarPlot"
+#include "QwtPolarGrid"
+#include <QwtPolarCurve>
+#include <QwtSymbol>
+
+const QVector<double> c_a = {0,45,90,135,180,225,270,315,360};
+//数据
+class EEGData : public QwtSeriesData<QwtPointPolar>
+{
+public:
+    EEGData()
+    {
+        assert(c_a.size()==9);
+
+        m_data.resize(9);
+        //测试数据
+        for(int i=0;i<9;i++){
+            m_data[i]=5;
+        }
+    }
+
+    size_t size() const override{
+        return 9;
+    }
+
+    void updateData(QVector<double> eeg){
+        //进行数据转换
+    }
+
+    QwtPointPolar sample(size_t i) const override{
+        assert(i>=0 && i<9);
+        const double a = c_a[i];
+        const double r = m_data[i];
+
+        return QwtPointPolar( a, r );
+    }
+
+    QRectF boundingRect() const override{
+        if(cachedBoundingRect.width() < 0.0){
+            cachedBoundingRect = qwtBoundingRect(*this);
+        }
+        return cachedBoundingRect;
+    }
+
+private:
+    QVector<double> m_data;
+};
+
+class EEGDataViewer : public QwtPolarPlot
 {
     Q_OBJECT
 public:
     explicit EEGDataViewer(QWidget *parent = nullptr);
 
+    void updateData(QVector<double> eeg);
+
 signals:
 
+private:
+    QwtPolarGrid *m_grid;
+
+    QwtPolarCurve *m_curve;
+
+    EEGData *data;
 };
 
 #endif // EEGDATAVIEWER_H
