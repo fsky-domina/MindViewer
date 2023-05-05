ac09313951287186ff5f35bccc2394bda4f58a2e
Jackey Lea <1768478912@qq.com>
2023-05-05 20:30:05 +0800

    添加信号显示功能

--- src/signaldataviewer.h
+++ src/signaldataviewer.h
@@ -1,8 +1,25 @@
+//////////////////////////
+/// JackeyLea
+/// 20230505
+/// 显示信号强度
+//////////////////////////
+
 #ifndef SIGNALDATAVIEWER_H
 #define SIGNALDATAVIEWER_H
 
 #include <QObject>
 #include <QWidget>
+#include <QPainter>
+#include <QPaintEvent>
+
+//信号强度
+enum SignalStrength{
+    Zero = 0,
+    One,
+    Two,
+    Three,
+    Four
+};
 
 class SignalDataViewer : public QWidget
 {
@@ -10,8 +27,16 @@ class SignalDataViewer : public QWidget
 public:
     explicit SignalDataViewer(QWidget *parent = nullptr);
 
+    //设置信号强度
+    void setSignalStrength(uchar s);
+
+protected:
+    void paintEvent(QPaintEvent *e);
+
 signals:
 
+private:
+    SignalStrength m_signalStrength;
 };
 
 #endif // SIGNALDATAVIEWER_H
